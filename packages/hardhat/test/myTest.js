b0a1958d93217373d3b8d4263aab8868ce0af9da
andrei.tarkovsky <mintcarkeys@protonmail.com>
2022-03-15 22:09:17 -0700

    Added tests
    Test to verify if a driver can go online.
    To verify if a request can be made.
    To verify if fare was transferred.

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -4,23 +4,93 @@ const { solidity } = require("ethereum-waffle");
 
 use(solidity);
 
-describe("My Dapp", function () {
+describe("DegenRides", function () {
   let myContract;
+  let accounts;
+
+  before(async () => {
+    accounts = await hre.ethers.getSigners();
+  });
+
+  beforeEach(async () => {
+    const YourContract = await ethers.getContractFactory("YourContract");
+
+    myContract = await YourContract.deploy(3560000000000000,
+      1200000000000,
+      (100 * 1000));
+  })
+
 
   describe("YourContract", function () {
-    it("Should deploy YourContract", async function () {
-      const YourContract = await ethers.getContractFactory("YourContract");
 
-      myContract = await YourContract.deploy();
-    });
+    describe("testdriverGoOnline()", function () {
+      it("Should be able to add a new driver", async function () {
+        const newPurpose = "Test Adding Driver";
 
-    describe("setPurpose()", function () {
-      it("Should be able to set a new purpose", async function () {
-        const newPurpose = "Test Purpose";
-
-        await myContract.setPurpose(newPurpose);
-        expect(await myContract.purpose()).to.equal(newPurpose);
+        expect(await myContract.getDriverCount()).to.equal(0);
       });
     });
+
+
+    describe("testdriverGoOnline()", function () {
+      it("Should be able to add a new driver", async function () {
+
+        await myContract.driverGoOnline(
+          3150000000000000,
+          35345643643,
+          "Taxi Driver",
+        )
+
+        expect(await myContract.getDriverCount()).to.equal(1);
+      });
+    });
+
+
+    describe("testRequestRide()", function () {
+      it("Should be able to add a request a ride", async function () {
+
+        await myContract.connect(accounts[2]).driverGoOnline(
+          3150000000000000,
+          35345643643,
+          "Taxi Driver",
+        )
+        expect(await myContract.getDriverCount()).to.equal(1);
+
+        await myContract.connect(accounts[1]).requestRide(
+          3150000000000000,
+          35345643643,
+          4150000000000000,
+          45345643643,
+          { value: 3560000000000000 },
+        )
+        expect(await myContract.getDriverCount()).to.equal(0);
+      });
+    });
+
+    describe("testRequestRide()", function () {
+      it("Correct fare is transferred to driver", async function () {
+
+        await myContract.connect(accounts[2]).driverGoOnline(
+          3150000000000000,
+          35345643643,
+          "Taxi Driver",
+        )
+
+        let driverBalance = await accounts[2].getBalance();
+
+        await myContract.connect(accounts[1]).requestRide(
+          3150000000000000,
+          35345643643,
+          4150000000000000,
+          45345643643,
+          { value: 3560000000000000 },
+        )
+
+        let driverBalanceAfterRide = await accounts[2].getBalance();
+        expect(driverBalanceAfterRide.gt(driverBalance)).to.equal(true);
+      });
+    });
+
+
   });
 });
